d6fcfc1b3e24c4004be54f2e0f624510d8898143
ArjunSingh748983 <singh.arjun2797@gmail.com>
2024-06-13 08:22:10 +0530

    yo

--- CV/resume.js
+++ CV/resume.js
@@ -5,8 +5,7 @@ let isDarkMode = true;
 
 button.addEventListener("click", function (e) {
   if (isDarkMode) {
-    body.style.background =
-      "radial-gradient(circle, rgb(239, 246, 249) 0%, rgb(206, 239, 253) 90%)"; // dark mode
+    body.style.background ="radial-gradient(circle, rgb(239, 246, 249) 0%, rgb(206, 239, 253) 90%)"; // dark mode
     body.style.color = "black";
     body.style.backgroundSize = "cover";
     links.forEach((link) => {
@@ -15,7 +14,7 @@ button.addEventListener("click", function (e) {
   } else {
     body.style.color = "white";
     body.style.background =
-      "radial-gradient(circle, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 100%);"; // Dark mode
+      "radial-gradient(circle, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 100%)"; // Dark mode
     body.style.backgroundSize = "cover";
     links.forEach((link) => {
       link.style.color = "rgb(72, 146, 220)";
